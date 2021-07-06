780038e509705127941d46fcb3f11d925a223378
Vishal Khandate <vishukblog000@gmail.com>
2021-07-06 16:16:26 +0530

    Backend almost ready

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const items = require("./routes/api/items");
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to mongo
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//Use Routes
+app.use("/api/items", items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server Started on port ${port}`);
+});
